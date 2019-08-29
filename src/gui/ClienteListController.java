0d284a6e3fd706c924513ebcde3e566682911a54
Flavio Lacks <flacks.cel@gmail.com>
2019-08-29 16:58:36 -0300

    Caixa de dialogo ClienteForm

--- src/gui/ClienteListController.java
+++ src/gui/ClienteListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Cliente;
 import model.services.ClienteService;
@@ -41,8 +50,9 @@ public class ClienteListController implements Initializable{
 	}
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Teste do click");
+	public void onBtNovoAction(ActionEvent evento) {
+		Stage parentStage = Utils.atualStage(evento);
+		criaDialogoFormu("/gui/ClienteForm.fxml", parentStage);
 	}
 	
 	private ObservableList<Cliente> obsList;
@@ -71,6 +81,25 @@ public class ClienteListController implements Initializable{
 		List<Cliente> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewCliente.setItems(obsList);
+	}
+	
+	private void criaDialogoFormu(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogoStage = new Stage();
+			dialogoStage.setTitle("Insira os dados do cliente");
+			dialogoStage.setScene(new Scene(pane));
+			dialogoStage.setResizable(false);
+			dialogoStage.initOwner(parentStage);
+			dialogoStage.initModality(Modality.WINDOW_MODAL);
+			dialogoStage.showAndWait();
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a visualização", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
