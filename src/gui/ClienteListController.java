0ce5cb2edffc5d9522d36a62d19b9eeed0df01b6
Flavio Lacks <flacks.cel@gmail.com>
2019-09-02 19:22:15 -0300

    Update Cliente

--- src/gui/ClienteListController.java
+++ src/gui/ClienteListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,75 +29,79 @@ import javafx.stage.Stage;
 import model.entities.Cliente;
 import model.services.ClienteService;
 
-public class ClienteListController implements Initializable, DataChangeListener{
-	
+public class ClienteListController implements Initializable, DataChangeListener {
+
 	private ClienteService service;
-	
+
 	@FXML
 	private TableView<Cliente> tableViewCliente;
-	
+
 	@FXML
 	private TableColumn<Cliente, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Cliente, String> tableColumnEmpresa;
-	
+
 	@FXML
 	private TableColumn<Cliente, String> tableColumnProjeto;
-	
+
+	@FXML
+	private TableColumn<Cliente, Cliente> tableColumnEDIT;
+
 	@FXML
 	private Button btNovo;
-	
+
 	public void setClienteService(ClienteService service) {
 		this.service = service;
 	}
-	
+
 	@FXML
 	public void onBtNovoAction(ActionEvent evento) {
 		Stage parentStage = Utils.atualStage(evento);
 		Cliente obj = new Cliente();
 		criaDialogoFormu(obj, "/gui/ClienteForm.fxml", parentStage);
 	}
-	
+
 	private ObservableList<Cliente> obsList;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		inicializaNode();
-		
+
 	}
 
 	private void inicializaNode() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnEmpresa.setCellValueFactory(new PropertyValueFactory<>("empresa"));
 		tableColumnProjeto.setCellValueFactory(new PropertyValueFactory<>("projeto"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewCliente.prefHeightProperty().bind(stage.heightProperty());
-		
+
 	}
-	
+
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service estava nulo!");
 		}
-		
+
 		List<Cliente> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewCliente.setItems(obsList);
+		initEditButtons();
 	}
-	
+
 	private void criaDialogoFormu(Cliente obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			ClienteFormController controller = loader.getController();
 			controller.setCliente(obj);
 			controller.setClienteService(new ClienteService());
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-			
+
 			Stage dialogoStage = new Stage();
 			dialogoStage.setTitle("Insira os dados do cliente");
 			dialogoStage.setScene(new Scene(pane));
@@ -103,9 +109,8 @@ public class ClienteListController implements Initializable, DataChangeListener{
 			dialogoStage.initOwner(parentStage);
 			dialogoStage.initModality(Modality.WINDOW_MODAL);
 			dialogoStage.showAndWait();
-			
-		}
-		catch(IOException e) {
+
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar a visualização", e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -113,7 +118,25 @@ public class ClienteListController implements Initializable, DataChangeListener{
 	@Override
 	public void onDataChanged() {
 		updateTableView();
-		
+
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Cliente, Cliente>() {
+			private final Button button = new Button("editar");
+
+			@Override
+			protected void updateItem(Cliente obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(	event -> criaDialogoFormu(obj, "/gui/ClienteForm.fxml", Utils.atualStage(event)));
+			}
+		});
 	}
 
 }
